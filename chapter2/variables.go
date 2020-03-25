5097530a8e7dfc02c2abb79f262a36da1a9ee03b
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-03-25 20:47:11 -0300

    Add variables and types.

---
+++ chapter2/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	fmt.Println(s)
+	var s1, s2, s3 string
+	fmt.Println(s1, s2, s3)
+	var s11, s22, s33 string = "first-string", "second-string", "thrid-string"
+	fmt.Println(s11, s22, s33)
+	var s4, s5, s6 string = "first-string", "second-string", "thrid-string"
+	fmt.Println(s4, s5, s6)
+	var s44, s55, s66 string = "first-string", "second-string", "thrid-string"
+	fmt.Println(s44, s55, s66)
+	
+	fmt.Println("Variables in go")
+}
