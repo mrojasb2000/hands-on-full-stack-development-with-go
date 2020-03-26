87593f36794174dfe63daedb765b8ed0c4ee1867
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-03-25 21:05:45 -0300

    Add pointers and types.

--- chapter2/pointers.go
+++ chapter2/pointers.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var x int = 5
-	fmt.Println("Value:   ", x)
+	fmt.Println("Value without pointer:   ", x)
 	var xptr = &x
-	fmt.Println("Address: ", xptr)
+	fmt.Println("Address pointer: ", xptr)
+	fmt.Println("Access to value from pointer:", *(xptr))
 }
